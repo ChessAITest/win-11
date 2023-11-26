b6511207514d0b3553c0b6b1e17bf341cd3dce93
ChessAITest <147644008+ChessAITest@users.noreply.github.com>
2023-11-26 18:18:21 -0500

    Update uv.config.js

--- uv/uv.config.js
+++ uv/uv.config.js
@@ -1,7 +1,7 @@
 /*global Ultraviolet*/
 self.__uv$config = {
     prefix: '/win-11/uv/service/',
-    bare: 'https://hello-world-round-bread-f221.shayaanhooda.workers.dev/',
+    bare: 'https://vdlf8p-8080.csb.app/bare/',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: '/win-11/uv/uv.handler.js',
