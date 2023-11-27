825664a787e54e57a54dce5310a6616de8cd006c
ChessAITest <147644008+ChessAITest@users.noreply.github.com>
2023-11-27 03:28:39 -0500

    Update uv.config.js

--- uv/uv.config.js
+++ uv/uv.config.js
@@ -1,7 +1,7 @@
 /*global Ultraviolet*/
 self.__uv$config = {
     prefix: '/win-11/uv/service/',
-    bare: 'https://vdlf8p-8080.csb.app/bare/',
+    bare: 'https://proxy.nolife.uk/bare/?__cpo=aHR0cHM6Ly92ZGxmOHAtODA4MC5jc2IuYXBw',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: '/win-11/uv/uv.handler.js',
